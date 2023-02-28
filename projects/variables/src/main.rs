5836f308eb3520f3041728a1b36084fbc54fcd82
Jan Szymanski <janohan@gmail.com>
2023-02-28 21:43:59 +0000

    some changes to the variable lesson

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("The value of x is: {x}");
+    //x = 6;
+    //println!("The value of x is: {x}");
+
+    {
+        let x = 12;
+        println!("The value of x in inner scope is: {x}");
+        //x = 15;
+        //println!("The value of x in inner scope again is: {x}");
+    }
+
     x = 6;
-    println!("The value of x is: {x}");
+    println!("The value of x is again: {x}");
 }
