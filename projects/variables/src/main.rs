1d2567c566eba585fa4383c126d524bb8936b4ee
Jan Szymanski <janohan@gmail.com>
2023-03-01 09:36:51 +0000

    Progressing through the book.

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -4,13 +4,27 @@ fn main() {
     //x = 6;
     //println!("The value of x is: {x}");
 
+    let j_str = "new string variable";
+    println!("The j_str val is: {}", j_str);
+
     {
-        let x = 12;
+        let mut x = 12;
         println!("The value of x in inner scope is: {x}");
         //x = 15;
         //println!("The value of x in inner scope again is: {x}");
+        x = 1;
+        println!("The value of x in inner scope again is: {x}");
+        let j_str = j_str.len();
+        println!("The j_str len was: {}", j_str);
     }
+    println!("The j_str val is again: {}", j_str);
 
     x = 6;
     println!("The value of x is again: {x}");
+
+    let tup: (i32, f32, u8) = (1024, 17.5, 7);
+
+    let (x, y, z) = tup;
+
+    println!("The values of x, y, z are now: {x}, {y}, {z}");
 }
