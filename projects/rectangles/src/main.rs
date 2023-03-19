81260f5e885b117446a804949281d3072d7abf0b
Jan Szymanski <janohan@gmail.com>
2023-03-19 11:17:14 +0000

    Moving work to big PC.

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -14,6 +14,7 @@ fn main() {
     let rect_area = rect1.area();
     println!("The area of given rectangle is {}", rect_area);
     println!("The rect1 properties are: \n{:#?}", rect1);
+    println!("The rect1 width is non zero: {}", rect1.width());
     println!("Can rect1 hold rect2: {}", rect1.can_hold(&rect2));
     println!("The rec3 sizes are w:{} h:{}", rect3.width, rect3.height);
 }
@@ -29,6 +30,9 @@ impl Rectangle {
     }
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.height > other.height && self.width > other.width
+    }
+    fn width(&self) -> bool {
+        self.width > 0
     }
     fn square(size: u32) -> Self {
         Self {
